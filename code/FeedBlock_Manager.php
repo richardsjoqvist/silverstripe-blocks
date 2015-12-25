9c0820dec03ded0c9213c17d1f1e0e5a37193576
helpfulrobot <chris@silverstripe.com>
2015-12-26 11:33:41 +1300

    Converted to PSR-2

--- code/FeedBlock_Manager.php
+++ code/FeedBlock_Manager.php
@@ -6,44 +6,45 @@
  */
 class FeedBlock_Manager extends GridField
 {
-			
-	/**
-	 * Create GridField for FeedBlocks
-	 */
-	function __construct($name, $title = null, SS_List $dataList = null, GridFieldConfig $config = null) {
-		if(!$config) {
-			$config = GridFieldConfig::create()->addComponents(
-				new GridFieldToolbarHeader(),
-				new GridFieldAddNewButton('toolbar-header-right'),
-				new GridFieldSortableHeader(),
-				new GridFieldDataColumns(),
-				new GridFieldPaginator(20),
-				new FeedBlock_GridFieldRefreshButton(),
-				new GridFieldEditButton(),
-				new GridFieldDeleteAction(),
-				new FeedBlock_GridFieldDetailForm()
-			);
-			if(count($dataList) > 1 && class_exists('GridFieldSortableRows')) {
-				$config->addComponent(new GridFieldSortableRows('SortOrder'));
-			}
-		}
-		parent::__construct($name, $title, $dataList, $config);
-	}
+            
+    /**
+     * Create GridField for FeedBlocks
+     */
+    public function __construct($name, $title = null, SS_List $dataList = null, GridFieldConfig $config = null)
+    {
+        if (!$config) {
+            $config = GridFieldConfig::create()->addComponents(
+                new GridFieldToolbarHeader(),
+                new GridFieldAddNewButton('toolbar-header-right'),
+                new GridFieldSortableHeader(),
+                new GridFieldDataColumns(),
+                new GridFieldPaginator(20),
+                new FeedBlock_GridFieldRefreshButton(),
+                new GridFieldEditButton(),
+                new GridFieldDeleteAction(),
+                new FeedBlock_GridFieldDetailForm()
+            );
+            if (count($dataList) > 1 && class_exists('GridFieldSortableRows')) {
+                $config->addComponent(new GridFieldSortableRows('SortOrder'));
+            }
+        }
+        parent::__construct($name, $title, $dataList, $config);
+    }
 
-	/**
-	 * Returns the whole gridfield rendered with all the attached components
-	 *
-	 * @param array $properties
-	 * @return string
-	 */
-	public function FieldHolder($properties = array()) {
-		Requirements::javascript(THIRDPARTY_DIR . '/jquery-livequery/jquery.livequery.min.js');
-		$modulePath = dirname(dirname(__FILE__));
-		$modulePath = str_replace(BASE_PATH, '', $modulePath);
-		$modulePath = substr($modulePath, 1);
-		Requirements::javascript($modulePath . '/js/feedblock_manager.js');
-		Requirements::css($modulePath . '/css/GridFieldRefreshButton.css');
-		return parent::FieldHolder($properties);
-	}
-
+    /**
+     * Returns the whole gridfield rendered with all the attached components
+     *
+     * @param array $properties
+     * @return string
+     */
+    public function FieldHolder($properties = array())
+    {
+        Requirements::javascript(THIRDPARTY_DIR . '/jquery-livequery/jquery.livequery.min.js');
+        $modulePath = dirname(dirname(__FILE__));
+        $modulePath = str_replace(BASE_PATH, '', $modulePath);
+        $modulePath = substr($modulePath, 1);
+        Requirements::javascript($modulePath . '/js/feedblock_manager.js');
+        Requirements::css($modulePath . '/css/GridFieldRefreshButton.css');
+        return parent::FieldHolder($properties);
+    }
 }
--- code/LinkBlock.php
+++ code/LinkBlock.php
@@ -6,92 +6,95 @@
  */
 class LinkBlock extends Block
 {
-	
-	/**
-	 * Fields
-	 *
-	 * @var array
-	 */
-	private static $db = array(
-		'CssClasses'	=> 'Text',
-		'Attributes'	=> 'Text',
-	);
+    
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    private static $db = array(
+        'CssClasses'    => 'Text',
+        'Attributes'    => 'Text',
+    );
 
-	/**
-	 * Summary fields for GridField
-	 *
-	 * @var array
-	 */
-	private static $summary_fields = array(
-		'Thumbnail'		=> 'Image',
-		'Title'			=> 'Title',
-		'URL'			=> 'URL',
- 	);
+    /**
+     * Summary fields for GridField
+     *
+     * @var array
+     */
+    private static $summary_fields = array(
+        'Thumbnail'        => 'Image',
+        'Title'            => 'Title',
+        'URL'            => 'URL',
+    );
 
-	/**
-	 * Get CMS fields
-	 *
-	 * @return FieldList
-	 */
-	function getCMSFields() {
-		$fields = new FieldList();
+    /**
+     * Get CMS fields
+     *
+     * @return FieldList
+     */
+    public function getCMSFields()
+    {
+        $fields = new FieldList();
 
-		$fields->push(new TextField('LinkTitle', _t('Block.LINKTITLE','Link title')));
-		$fields->push(new TextField('LinkExternal', _t('Block.LINKEXTERNAL','External link URL')));
+        $fields->push(new TextField('LinkTitle', _t('Block.LINKTITLE', 'Link title')));
+        $fields->push(new TextField('LinkExternal', _t('Block.LINKEXTERNAL', 'External link URL')));
 
-		if(class_exists('OptionalTreeDropdownField')) {
-			$treeField = new OptionalTreeDropdownField('LinkInternalID', _t('Block.LINKINTERNAL','Internal link'), 'SiteTree');
-			$treeField->setEmptyString('No Page');
-		}
-		else {
-			$treeField = new TreeDropdownField('LinkInternalID', _t('Block.LINKINTERNAL','Internal link'), 'SiteTree');
-		}
-		$fields->push($treeField);
+        if (class_exists('OptionalTreeDropdownField')) {
+            $treeField = new OptionalTreeDropdownField('LinkInternalID', _t('Block.LINKINTERNAL', 'Internal link'), 'SiteTree');
+            $treeField->setEmptyString('No Page');
+        } else {
+            $treeField = new TreeDropdownField('LinkInternalID', _t('Block.LINKINTERNAL', 'Internal link'), 'SiteTree');
+        }
+        $fields->push($treeField);
 
-		$fields->push(new TextField('CssClasses', _t('Block.CSSCLASSES','Css Classes')));
-		$fields->push(new TextareaField('Attributes', _t('Block.ATTRIBUTES','Attributes')));
+        $fields->push(new TextField('CssClasses', _t('Block.CSSCLASSES', 'Css Classes')));
+        $fields->push(new TextareaField('Attributes', _t('Block.ATTRIBUTES', 'Attributes')));
 
-		$imageField = new UploadField('Image', _t('Block.IMAGE','Image'));
-		$imageField->getValidator()->setAllowedExtensions(array('jpg', 'gif', 'png'));
-		$fields->push($imageField);
+        $imageField = new UploadField('Image', _t('Block.IMAGE', 'Image'));
+        $imageField->getValidator()->setAllowedExtensions(array('jpg', 'gif', 'png'));
+        $fields->push($imageField);
 
-		return $fields;
-	}
+        return $fields;
+    }
 
-	/**
-	 * Get link url
-	 *
-	 * @return string
-	 */
-	function URL() {
-		return parent::LinkURL();
-	}
-	
-	/**
-	 * Get link title
-	 *
-	 * @return string
-	 */
-	function Title() {
-		return parent::LinkTitle();
-	}
-	
-	/**
-	 * Get link is external
-	 *
-	 * @return bool
-	 */
-	function IsExternal() {
-		return parent::LinkIsExternal();
-	}
+    /**
+     * Get link url
+     *
+     * @return string
+     */
+    public function URL()
+    {
+        return parent::LinkURL();
+    }
+    
+    /**
+     * Get link title
+     *
+     * @return string
+     */
+    public function Title()
+    {
+        return parent::LinkTitle();
+    }
+    
+    /**
+     * Get link is external
+     *
+     * @return bool
+     */
+    public function IsExternal()
+    {
+        return parent::LinkIsExternal();
+    }
 
-	/**
-	 * Get link is internal
-	 *
-	 * @return bool
-	 */
-	function IsInternal() {
-		return !self::IsExternal();
-	}
-	
+    /**
+     * Get link is internal
+     *
+     * @return bool
+     */
+    public function IsInternal()
+    {
+        return !self::IsExternal();
+    }
 }
